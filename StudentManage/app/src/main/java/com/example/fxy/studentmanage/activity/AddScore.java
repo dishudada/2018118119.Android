793e57d47d3223cbcf28fd411be7df9e0acbfef0
dishudada <1660955971@qq.com>
2020-12-24 21:20:01 +0800

    安卓大作业

---
+++ StudentManage/app/src/main/java/com/example/fxy/studentmanage/activity/AddScore.java
@@ -0,0 +1,12 @@
+package com.example.fxy.studentmanage.activity;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.fxy.studentmanage.R;
+
+public class AddScore extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.studentinformation);
+	}
+}
---
+++ StudentManage/app/src/main/java/com/example/fxy/studentmanage/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fxy.studentmanage.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fxy.studentmanage.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
